1be1c5e9093dbd85ef9c5760c840014d82091bad
byron <90ksoon@gmail.com>
2022-08-19 00:08:46 +0800

    deploy

---
+++ assets/20220818-does-your-code-base-smell.78681dea.js
@@ -0,0 +1,2 @@
+const o={Author:"Byron Wong",Date:"2022-08-18T00:00:00.000Z",Slug:"does-your-code-base-smell"},e=`<h1>Does Your Code Base Smell</h1>
+`;export{o as attributes,e as html};
---
+++ assets/index.e40cb0aa.js
@@ -0,0 +1,1 @@
+const e={blogs:{"20220818-does-your-code-base-smell":{filename:"20220818-does-your-code-base-smell",slug:"does-your-code-base-smell",title:"Does Your Code Base Smell"}}};export{e as default};
