d134140a0c04888bb2ac75d9fee54ce4eaa952f0
byron <90ksoon@gmail.com>
2022-08-24 22:33:02 +0800

    deploy

--- assets/20220824-strangling-your-tech-debt.df474881.js
+++ assets/20220824-strangling-your-tech-debt.df474881.js
@@ -10,7 +10,7 @@ const e={Author:"Byron Wong",Date:"2022-08-24T00:00:00.000Z",Slug:"strangling-yo
 <p>As we all hiking on this steep climb of attaining artisanship, away from lowly mediocrity in our crafts, we need tools and solutions.</p>
 <p>It's in my interest in sharing and getting ideas and feedbacks from fellow SWE so that I may <em>git gud</em>. Hence with this as my first post in an attempt to document my findings, sharing stories and improve my writing haha</p>
 <h3>I'm imploring the readers to take this journey with me, as I journal the trade-offs, ideas and overall skill-improving practices that we all can take a bigger stride and leap forward as artisans in our crafts</h3>
-<p>I'll be writing occasionally, hopefully in a smaller bite-size content, I really didn't want to bore everyone :)</p>
+<p>I'll be writing occasionally, hopefully in a smaller bite-size content, I really didn't want to bore everyone \u{1F603}</p>
 <p><em>Thoughts on upcoming topics to journal:</em></p>
 <ul>
 <li>Introducing tests on low coverage code base (In the case of Small Medium Business Projects)</li>
