164ccced97615be5c1a44ad6efcdfb521534c813
byron <90ksoon@gmail.com>
2022-08-24 21:42:42 +0800

    deploy

---
+++ assets/index.f8312c43.js
@@ -0,0 +1,1 @@
+const e={blogs:{"20220824-stragling-your-tech-debt":{filename:"20220824-stragling-your-tech-debt",slug:"strangling-your-tech-debt-and-squeeze-out-any-usefulness-from-it",title:"Strangling your tech debt and squeeze out any usefulness from it!",createdAt:"24th Aug 2022"}}};export{e as default};
