4039bac606855472ce1e3913f94e198b51669d69
byron <90ksoon@gmail.com>
2022-08-25 21:47:18 +0800

    deploy

--- assets/20220824-strangling-your-tech-debt.dbb0ea2b.js
+++ assets/20220824-strangling-your-tech-debt.dbb0ea2b.js
@@ -1,16 +1,16 @@
 const e={Author:"Byron Wong",Date:"2022-08-24T00:00:00.000Z",Slug:"strangling-your-tech-debt-and-squeeze-out-any-usefulness-from-it"},t=`<h1>Strangling your tech debt and squeeze out any usefulness from it !</h1>
-<p>When we have a sensible service (big or small) starts failing on you.</p>
+<p>When a / multiple sensible service(s) (big or small) starts failing on you.</p>
 <p>The stench from the code base arose from the foul spaghetti monster, who happened to manifest itself from the long neglected care and inconsistent upkeeping;</p>
-<p>Aren't something that we are willing to witness</p>
+<p>Aren't something that we are willing to witness.</p>
 <p>But the eventuality that it surfaces is inevitable, and we have to do something about it:</p>
 <blockquote>
 <p>It's no longer will it happen, but <em>when</em> will happen?</p>
 </blockquote>
 <p>Let's not play the blame game, 'cause sometimes it might be <strong>US</strong> that's the faulter (yes at some point we're all guilty of it)</p>
 <p>As we all hiking on this steep climb of attaining artisanship, away from lowly mediocrity in our crafts, we need tools and solutions.</p>
-<p>It's in my interest in sharing and getting ideas and feedbacks from fellow SWE so that I may <em>git gud</em>. Hence with this as my first post in an attempt to document my findings, sharing stories and improve my writing haha</p>
+<p>It's in my interest in sharing and getting ideas and feedbacks from fellow SWE so that I may <em>git gud</em>. Hence with this as my first post in an attempt to document my findings, sharing stories and improve my writing haha.</p>
 <h3>I'm imploring the readers to take this journey with me, as I journal the trade-offs, ideas and overall skill-improving practices that we all can take a bigger stride and leap forward as artisans in our crafts</h3>
-<p>I'll be writing occasionally, hopefully in a smaller bite-size content, I really didn't want to bore everyone \u{1F913}</p>
+<p>I'd be writing occasionally, hopefully in a smaller bite-size content, I really didn't want to bore everyone \u{1F913}</p>
 <p><em>Thoughts on upcoming topics to journal:</em></p>
 <ul>
 <li>Introducing tests on low coverage code base (In the case of Small Medium Business Projects)</li>
