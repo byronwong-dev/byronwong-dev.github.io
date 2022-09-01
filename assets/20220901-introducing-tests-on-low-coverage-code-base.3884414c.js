fabfafdba930b6afddbc7b35a5665db6da52930e
byron <90ksoon@gmail.com>
2022-09-01 21:47:42 +0800

    deploy

--- assets/20220901-introducing-tests-on-low-coverage-code-base.3884414c.js
+++ assets/20220901-introducing-tests-on-low-coverage-code-base.3884414c.js
@@ -1,8 +1,10 @@
 const e={Author:"Byron Wong",Date:"2022-09-01T00:00:00.000Z",Slug:"introducing-tests-on-low-coverage-code-base",Keywords:"test, automation, cicd, unit-test, feature-test"},t=`<h1>Introducing Tests On Low Coverage Code Base</h1>
+<p>If I ask you, what's the bread and butter in your code base?</p>
+<p>Git version control? Linter? true to those, but equally important element would be -- your tests!</p>
 <blockquote>
-<h3><em>In my humble opinion, if you do not introduce test in your code base, and not maintaining the test, you're like eating pizza without dough</em></h3>
+<h3><em>IMO, if you do not introduce test in your code base, and not maintaining the tests, you're like eating pizza without dough</em></h3>
 </blockquote>
-<p>The basic bread and butter in your everyday meal is gone, at the very moment you do not realize or ditched the use of tests in your code base. &quot;Lacking of nutrients&quot; is how I would put it.</p>
+<p>If you do not realize or ditched the use of tests in your code base. &quot;Lacking of nutrients&quot; is how I would put it.</p>
 <p>Why?</p>
 <p>Simple, you won't have the energy or confidence to enable you in your journey to &quot;scale&quot;, &quot;transform&quot;, &quot;revolutionize&quot; or &quot;disrupt the industry&quot; \u{1F64A}</p>
 <p>Let's take for instance, if you have a team of 5-6 developers, how do you ensure whatever deployed is working as-is? How do you sleep at night not thinking one day something might happen and you incorrectly gave out money to your customer?</p>
