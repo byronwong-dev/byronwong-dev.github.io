a34f2be6d86010cd21e9cc0102f517ce851a2180
byron <90ksoon@gmail.com>
2022-09-01 21:13:22 +0800

    deploy

--- assets/20220901-introducing-tests-on-low-coverage-code-base.21ad1e6b.js
+++ assets/20220901-introducing-tests-on-low-coverage-code-base.21ad1e6b.js
@@ -5,5 +5,7 @@ const o={Author:"Byron Wong",Date:"2022-09-01T00:00:00.000Z",Slug:"introducing-t
 <p>The basic bread and butter in your everyday meal is gone, at the very moment you do not realize or ditched the use of tests in your code base. &quot;Lacking of nutrients&quot; is how I would put it.</p>
 <p>Why?</p>
 <p>Simple, you won't have the energy or confidence to enable you in your journey to &quot;scale&quot;, &quot;transform&quot;, &quot;revolutionize&quot; or &quot;disrupt the industry&quot; \u{1F64A}</p>
-<p>Let's take for instance, if you have a team of 5-6 developers, how do you ensure whatever deployed is working as is? If that's not enough to convince you, perhaps I could point you to a direction where you should search &quot;why do i write test&quot;</p>
+<p>Let's take for instance, if you have a team of 5-6 developers, how do you ensure whatever deployed is working as-is? How do you sleep at night not thinking one day something might happen and you incorrectly gave out money to your customer?</p>
+<p>If that's not enough to convince you, perhaps I could point you to google &quot;why do i write test&quot;, come back here when you're otherwise <strong><em>convinced</em></strong>.</p>
+<p>Now I'm not attempting to persuade you (_pardon me for what I've written prior), nor plan to write a detailed guide on how to write test, believe me, <em>&quot;any test is good test&quot;</em>.</p>
 `;export{o as attributes,e as html};
