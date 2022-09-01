4807d0bfbdffcf3f18502b809b85ffc884fe3137
byron <90ksoon@gmail.com>
2022-09-01 21:09:57 +0800

    deploy

--- assets/20220901-introducing-tests-on-low-coverage-code-base.b4feebd1.js
+++
@@ -1,5 +0,0 @@
-const t={Author:"Byron Wong",Date:"2022-09-01T00:00:00.000Z",Slug:"introducing-tests-on-low-coverage-code-base",Keywords:"test, automation, cicd, unit-test, feature-test"},o=`<h1>Introducing Tests On Low Coverage Code Base</h1>
-<blockquote>
-<p>In my humble opinion, if you do not introduce test in your code base, and not maintaining the test, you're like eating pizza without dough</p>
-</blockquote>
-`;export{t as attributes,o as html};
